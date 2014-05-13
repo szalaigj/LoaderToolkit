aaee7c73d7ad3507d761b8a2a0b2b89865c2a439
szalaigj <szalai@complex.elte.hu>
2014-05-13 10:22:36 +0200

    Correct the location of keyword 'with'

--- sql/merge_ref.sql
+++ sql/merge_ref.sql
@@ -1,4 +1,4 @@
 dbcc traceon (610);
 
-INSERT INTO [$targetdb].[dbo].[ref] ([refID],[pos],[refNuc]) WITH(TABLOCK)
+INSERT INTO [$targetdb].[dbo].[ref] WITH(TABLOCK) ([refID],[pos],[refNuc])
 SELECT [refID],[pos],[refNuc] FROM [$loaddb].[dbo].[$tablename];
