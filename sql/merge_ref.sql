bbd059b51475f27fe1e3dfe0c475ba3e9416924a
szalaigj <szalai@complex.elte.hu>
2014-05-13 10:11:03 +0200

    Extend merging script with hint tablock

--- sql/merge_ref.sql
+++ sql/merge_ref.sql
@@ -1,4 +1,4 @@
 dbcc traceon (610);
 
-INSERT INTO [$targetdb].[dbo].[ref] ([refID],[pos],[refNuc])
+INSERT INTO [$targetdb].[dbo].[ref] ([refID],[pos],[refNuc]) WITH(TABLOCK)
 SELECT [refID],[pos],[refNuc] FROM [$loaddb].[dbo].[$tablename];
