5eb8e0414be8845c483f13d97e649a83bf8aab2e
szalaigj <szalai@complex.elte.hu>
2014-05-14 18:20:37 +0200

    Add new skeleton mapper and merger for sread.

---
+++ LoaderToolkit/BatchLoader/Mappers/Sread.cs
@@ -0,0 +1,22 @@
+﻿using LoaderLibrary.Load;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BatchLoader.Mappers
+{
+    public class Sread : Mapper<string>
+    {
+        public override string TableName
+        {
+            get { return "sreadLoad"; }
+        }
+
+        public override void Map(string obj)
+        {
+
+        }
+    }
+}
---
+++ LoaderToolkit/BatchLoader/Mergers/Sread.cs
@@ -0,0 +1,22 @@
+﻿using LoaderLibrary.Load;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BatchLoader.Mergers
+{
+    public class Sread : Merger
+    {
+        protected override string SourceTableName
+        {
+            get { return "sreadLoad"; }
+        }
+
+        protected override string TargetTableName
+        {
+            get { return "sread"; }
+        }
+    }
+}
