2cbf8842b32bf7cb885dd4fa0f859e52a25d43ca
szalaigj <szalai@complex.elte.hu>
2014-07-10 09:52:32 -0400

    Add parameter @refVar to queries.

--- sql/real_queries.sql
+++ sql/real_queries.sql
@@ -3,6 +3,8 @@ USE szalaigj
 GO
 
 -- This sample query results nucleotide in position 93696 (of reference with refID 65539):
+DECLARE @refVar int
+SELECT @refVar = 65539
 DECLARE @posVar bigint
 SELECT @posVar = 93696
 DECLARE @refPosStartVar bigint
@@ -15,9 +17,11 @@ SELECT [refID]
       ,[seqBlock]
 	  ,[dbo].[DetDecRefSeq](@radius, @posVar, [posStart], [seqBlock]) as decSeq
   FROM [dbo].[refBin] rb
-WHERE rb.[refID] = 65539 AND rb.[posStart] = @refPosStartVar
+WHERE rb.[refID] = @refVar AND rb.[posStart] = @refPosStartVar
 
 -- This sample query finds neighbourhood for position 93696 (of reference with refID 65539) when the radius is two:
+DECLARE @refVar int
+SELECT @refVar = 65539
 DECLARE @posVar bigint
 SELECT @posVar = 93696
 DECLARE @refPosStartVar bigint
@@ -30,4 +34,4 @@ SELECT [refID]
       ,[seqBlock]
 	  ,[dbo].[DetDecRefSeq](@radius, @posVar, [posStart], [seqBlock]) as decSeq
   FROM [dbo].[refBin] rb
-WHERE rb.[refID] = 65539 AND rb.[posStart] = @refPosStartVar
+WHERE rb.[refID] = @refVar AND rb.[posStart] = @refPosStartVar
