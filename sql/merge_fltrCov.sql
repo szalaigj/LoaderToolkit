8117386b3cbb94c3adb44eb264f62c56636d3940
szalaigj <szalai@complex.elte.hu>
2014-10-24 18:06:40 -0400

    Extend insert into with "WITH(TABLOCKX)"

--- sql/merge_fltrCov.sql
+++ sql/merge_fltrCov.sql
@@ -19,7 +19,7 @@ WHERE fdpLd.sampleName = s.name
 AND fdpLd.speciesID = (rd.refID & 0xFFFF0000) / POWER(2, 16)
 AND fdpLd.extID = rd.extID;
 
-INSERT INTO [$targetdb].[dbo].[fltrCov] (pupID, pos, refNuc, coverage, bases, basesQual)
+INSERT INTO [$targetdb].[dbo].[fltrCov] WITH(TABLOCKX) (pupID, pos, refNuc, coverage, bases, basesQual)
 SELECT pup.pupID, fdpLd.pos, fdpLd.refNuc, fdpLd.coverage, fdpLd.bases, fdpLd.basesQual
   FROM [$loaddb].[dbo].[$tablename] as fdpLd, [$targetdb].[dbo].[sample] as s, [$targetdb].[dbo].[refDesc] as rd, [$targetdb].[dbo].[pileup] as pup
   WHERE fdpLd.sampleName = s.name
